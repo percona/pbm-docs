fc326a0a1607d05e338f86ac23cd4c477f59d56f
Anastasia Alexadrova <anastasia.alexandrova@percona.com>
2026-01-22 10:11:33 +0100

    Deleted unnecessary non-related stuff

--- usr/pgsql-17/share/extension/pg_cron--1.4-1--1.5.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE cron.job ALTER COLUMN jobname TYPE text;
-
-DROP FUNCTION cron.unschedule(name);
-CREATE FUNCTION cron.unschedule(job_name text)
-    RETURNS bool
-    LANGUAGE C STRICT
-    AS 'MODULE_PATHNAME', $$cron_unschedule_named$$;
-COMMENT ON FUNCTION cron.unschedule(text)
-    IS 'unschedule a pg_cron job';
